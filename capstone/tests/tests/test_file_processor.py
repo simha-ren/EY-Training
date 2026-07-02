ecee7c3a6368cc24dedbccb523339a9164ab28cf
simha-ren <simhagoud86@gmail.com>
2026-07-02 16:28:59 +0530

    Delete capstone/tests/tests directory

--- capstone/tests/tests/test_file_processor.py
+++
@@ -1,6 +0,0 @@
-from core.core.file_processor import FileProcessor
-
-def test_supported_extensions():
-    assert FileProcessor.is_supported("a.md")
-    assert FileProcessor.is_supported("a.pdf")
-    assert not FileProcessor.is_supported("a.zip")
